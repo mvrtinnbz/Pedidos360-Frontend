f24b9f9314288e993df281caa31b41a52c6aec14
mvrtinnbz <martinbazas2004@gmail.com>
2026-09-09 22:49:51 -0300

    Version 1.4.1: Fix: Direccion de Api Gateway actualizada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://rh9wpp19s0.execute-api.us-east-1.amazonaws.com/desarrollo',
-  frontendUrl: 'https://TU-DOMINIO-FRONTEND.nip.io' // lo completamos cuando tengas la 4ª EC2 y el certificado
+  frontendUrl: 'https://17v0lg9bh9.execute-api.us-east-1.amazonaws.com/desarrollo' 
 };
