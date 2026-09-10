7f832983e385e490210ea763ba9d687dcba41878
mvrtinnbz <martinbazas2004@gmail.com>
2026-09-09 22:58:55 -0300

    Version 1.4.2: Fix: Direccion de Frontend actualizada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://rh9wpp19s0.execute-api.us-east-1.amazonaws.com/desarrollo',
-  frontendUrl: 'https://17v0lg9bh9.execute-api.us-east-1.amazonaws.com/desarrollo' 
+  frontendUrl: 'https://17v0lg9bh9.execute-api.us-east-1.amazonaws.com/desarrollo/' 
 };
